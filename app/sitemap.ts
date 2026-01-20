44af7d9bafcb2f4e0e9e9da05f05303321868301
ishanjarwal <ishan.professional01@gmail.com>
2026-01-20 19:34:27 +0530

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,14 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://jarwal.vercel.app'
+  const baseUrl = "https://jarwal.vercel.app";
 
-    return [
-        {
-            url: baseUrl,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1,
-        },
-        {
-            url: 'https://drive.google.com/file/d/1zIEmKAidyXH4XyLrGuMI0cb2sRCACF8a/view',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-    ]
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
 }
